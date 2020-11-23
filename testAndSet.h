596c53bf9160a9866710abf4629de539d1087528
depren <nicolas.depre@student.uclouvain.be>
2020-11-23 19:19:09 +0100

    some updates

--- testAndSet.h
+++ testAndSet.h
@@ -4,5 +4,9 @@
 
 #ifndef PERFORMANCEEVALUATIONMULTITHREAD_TESTANDSET_H
 #define PERFORMANCEEVALUATIONMULTITHREAD_TESTANDSET_H
+typedef struct sl spinLock;
+void spinlock_init(spinLock**);
+void spinlock_lock(spinLock** lock);
+void spinlock_unlock(spinLock** lock);
 
 #endif //PERFORMANCEEVALUATIONMULTITHREAD_TESTANDSET_H
