36d3a60b5f2c7e7e5e314e8419057bcf643670ab
NicolasDepre <niclas.depre@student.uclouvain.be>
2020-11-27 11:03:35 +0100

    script

---
+++ script.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "RESULTATS: $1" > $2
+for i in $(seq 1 1 16)
+do
+	echo $i
+	for j in $(seq 1 1 5)
+	do
+		echo '    ' $j
+		(/usr/bin/time $1 $i $i) 2>&1 | awk '{print $3}' | grep 'elapsed' | cut --complement -c8-14 | tr -d '\n' >> $2
+		echo ','| tr -d '\n' >>$2
+	done
+	echo '' >> $2
+done
