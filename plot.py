1648b42810fbc1612f39eee5202cb112cf48770e
NicolasDepre <niclas.depre@student.uclouvain.be>
2020-12-02 19:56:47 +0100

    plt

--- plot.py
+++ plot.py
@@ -1,19 +1,20 @@
 from matplotlib import pyplot as plt
 import numpy as np
+X = np.arange(1,9)
+FILES =[] #Mettre les path des fichiers csv à plot
+for file in FILES;
+    Y = []
+    with open("file","r") as file:
+        file.readline()
+        for line in file.readlines():
+            line=line.strip().split(',')
+            temp = [float((k[2:].strip('0'))) for k in line if k != ""]
+            Y.append(np.mean(temp))
+    plt.plot(X,Y)
 
-Y = []
-with open("c-presults.csv","r") as file:
-    file.readline()
-    for line in file.readlines():
-        print(line)
-        line=line.strip().split(',')
-        temp = [float((k[2:].strip('0'))) for k in line if k != ""]
-        Y.append(np.mean(temp))
-print("y:")
-print(Y)
-X = np.arange(1,17)
-
-plt.figure()
-plt.plot(X,Y)
-plt
+plt.title("TITLE")
+plt.ylabel("Temps d'exécution (s)")
+plt.xlabel("Threads")
+plt.grid()
+plt.legend([],loc='upper right') 
 plt.show()
